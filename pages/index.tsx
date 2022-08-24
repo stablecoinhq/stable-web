6e50c0b72ebf2c141e37ec29d0e71788f888bb56
kyori19 <kyori@accelf.net>
2022-08-24 20:22:12 +0900

    Fix imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Image from 'next/image';
+
 import styles from 'styles/Home.module.scss';
+
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => (
