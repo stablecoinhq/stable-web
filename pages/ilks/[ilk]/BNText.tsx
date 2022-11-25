6bcd804f2981e0ede70c844fbe9c4dc107a842ef
Hiroto Shioi <shioihigg@gmail.com>
2022-11-25 15:16:20 +0900

    [STAB-164] Add tooltip text (#31)

--- pages/ilks/[ilk]/BNText.tsx
+++ pages/ilks/[ilk]/BNText.tsx
@@ -1,4 +1,4 @@
-import { Grid, TextField } from '@mui/material';
+import { Grid, TextField, Tooltip } from '@mui/material';
 
 import type { FixedNumber } from 'ethers';
 import type { FC, ReactNode } from 'react';
@@ -6,12 +6,15 @@ import type { FC, ReactNode } from 'react';
 export type BNTextProps = {
   label: ReactNode;
   value: FixedNumber;
+  tooltipText: NonNullable<ReactNode>;
 };
 
-const BNText: FC<BNTextProps> = ({ label, value }) => (
-  <Grid item xs={6}>
-    <TextField variant="standard" fullWidth label={label} value={value.toString()} inputProps={{ disabled: true }} />
-  </Grid>
+const BNText: FC<BNTextProps> = ({ label, value, tooltipText: helperText }) => (
+  <Tooltip title={helperText} arrow>
+    <Grid item xs={6}>
+      <TextField variant="standard" fullWidth label={label} value={value.toString()} inputProps={{ disabled: true }} />
+    </Grid>
+  </Tooltip>
 );
 
 export default BNText;
