6b2045067c5dd82ca17f6dfbf23ae479519e75c5
Hiroto Shioi <shioihigg@gmail.com>
2022-12-01 16:34:55 +0900

    Add hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+require("dotenv").config({ path: path.resolve(process.cwd(), '.env.local') });
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      },
+    },
+  },
+};
