dd827561fe841f87a1ae7f74a1d5929cd3c097ab
Hiroto Shioi <shioihigg@gmail.com>
2022-12-07 16:19:41 +0900

    [STAB 67] Burn validation (#43)

--- pages/forms/BurnForm.tsx
+++ pages/forms/BurnForm.tsx
@@ -1,21 +1,28 @@
-import { Button, Card, Grid, InputAdornment, TextField, CircularProgress } from '@mui/material';
+import { Button, Card, Grid, InputAdornment, TextField, CircularProgress, FormHelperText } from '@mui/material';
 import { useTranslation } from 'next-i18next';
 import { useCallback, useMemo, useState } from 'react';
 
 import { UnitFormats } from 'ethereum/helpers/math';
 import { cutDecimals, pickNumbers, toFixedNumberOrUndefined } from 'ethereum/helpers/stringNumber';
 
+import { BurnFormValidation, BurnError } from './BurnFormValidation';
+
 import type { IlkInfo } from 'ethereum/contracts/IlkRegistryHelper';
+import type { IlkStatus } from 'ethereum/contracts/VatHelper';
 import type { FixedNumber } from 'ethers';
 import type { FC, ChangeEventHandler, MouseEventHandler, ReactNode } from 'react';
 
 export type BurnFormProps = {
+  daiBalance: FixedNumber;
+  lockedBalance: FixedNumber;
+  debt: FixedNumber;
+  ilkStatus: IlkStatus;
   ilkInfo: IlkInfo;
   buttonContent: ReactNode;
   onBurn: (daiAmount: FixedNumber, colAmount: FixedNumber) => Promise<void>;
 };
 
-const BurnForm: FC<BurnFormProps> = ({ ilkInfo, onBurn, buttonContent }) => {
+const BurnForm: FC<BurnFormProps> = ({ ilkInfo, onBurn, buttonContent, daiBalance, lockedBalance, debt, ilkStatus }) => {
   const { t } = useTranslation('common', { keyPrefix: 'forms.burn' });
   const [daiText, setDaiText] = useState('');
   const daiAmount = useMemo(() => toFixedNumberOrUndefined(daiText, UnitFormats.WAD), [daiText]);
@@ -43,6 +50,26 @@ const BurnForm: FC<BurnFormProps> = ({ ilkInfo, onBurn, buttonContent }) => {
     });
   }, [colAmount, daiAmount, onBurn]);
 
+  const formErrors: BurnError[] = useMemo(() => {
+    if (colAmount && daiAmount) {
+      return BurnFormValidation.canBurn(daiBalance, lockedBalance, debt, colAmount, daiAmount, ilkStatus);
+    }
+    return [];
+  }, [colAmount, daiAmount, lockedBalance, ilkStatus, debt, daiBalance]);
+  const showErrorMessage = (e: BurnError) => {
+    switch (e) {
+      case BurnError.insufficientBalance:
+        return t('error.insufficientBalance');
+      case BurnError.collateralTooLow:
+        return t('error.collateralTooLow');
+      case BurnError.debtTooLow:
+        return t('error.debtTooLow');
+      case BurnError.invalidCollateralFreeAmount:
+        return t('error.invalidCollateralFreeAmount');
+      case BurnError.invalidRepayAmount:
+        return t('error.invalidRepayAmount');
+    }
+  };
   return (
     <Card component="form" elevation={0}>
       <Grid container padding={2} spacing={2}>
@@ -70,9 +97,19 @@ const BurnForm: FC<BurnFormProps> = ({ ilkInfo, onBurn, buttonContent }) => {
         </Grid>
 
         <Grid item xs={12}>
-          <Button variant="contained" fullWidth disabled={!daiAmount || !colAmount || burning} onClick={onButtonClick}>
+          <Button
+            variant="contained"
+            fullWidth
+            disabled={!daiAmount || !colAmount || burning || formErrors.length !== 0}
+            onClick={onButtonClick}
+          >
             {burning ? <CircularProgress /> : buttonContent}
           </Button>
+          {formErrors.map((e) => (
+            <FormHelperText key={e} error>
+              {showErrorMessage(e)}
+            </FormHelperText>
+          ))}
         </Grid>
       </Grid>
     </Card>
---
+++ pages/forms/BurnFormValidation.ts
@@ -0,0 +1,113 @@
+import { UnitFormats } from 'ethereum/helpers/math';
+
+import type { IlkStatus } from 'ethereum/contracts/VatHelper';
+import type { FixedNumber } from 'ethers';
+
+export enum BurnError {
+  // ウォレットのDAIの残高が不足している
+  insufficientBalance,
+  // 過剰返済。返済額が負債額を超えていないか
+  invalidRepayAmount,
+  // 引き出す担保がロックされている担保を上回っている
+  invalidCollateralFreeAmount,
+  // vaultの負債が下限値を下回っている
+  debtTooLow,
+  // vaultの担保率が最低担保率を下回っている
+  collateralTooLow,
+}
+
+const format = UnitFormats.RAD;
+
+export class BurnFormValidation {
+  /**
+   * Burnが可能か検証を行う。できない場合にはその理由をリストとして返す
+   * @param daiBalance ウォレットに保有するDAIの数量
+   * @param lockedBalance 既にVaultにロックされている担保 (Vault.urn.ink)
+   * @param debt Vaultが負っている負債 (Vault.urn.art)
+   * @param collateralToFree 引き出そうとしている担保の数量
+   * @param daiToRepay 返済しようとしているDAIの数量
+   * @param ilkStatus 任意の担保に関する情報 (Vat.ilk)
+   */
+  static canBurn(
+    daiBalance: FixedNumber,
+    lockedBalance: FixedNumber,
+    debt: FixedNumber,
+    collateralToFree: FixedNumber,
+    daiToRepay: FixedNumber,
+    ilkStatus: IlkStatus,
+  ): BurnError[] {
+    const { debtMultiplier, price, debtFloor } = ilkStatus;
+
+    const errors: BurnError[] = [];
+
+    if (BurnFormValidation.isInsufficientBalance(daiBalance, daiToRepay)) {
+      errors.push(BurnError.insufficientBalance);
+    }
+
+    const currentDebt = BurnFormValidation.getCurrentDebt(daiToRepay, debt, debtMultiplier);
+    if (BurnFormValidation.isOverRepaying(debt, daiToRepay, debtMultiplier)) {
+      errors.push(BurnError.invalidRepayAmount);
+    }
+
+    if (BurnFormValidation.isInvalidCollateralFreeAmount(lockedBalance, collateralToFree)) {
+      errors.push(BurnError.invalidCollateralFreeAmount);
+    }
+
+    if (BurnFormValidation.isCollateralizationRatioTooLow(lockedBalance, collateralToFree, currentDebt, price)) {
+      errors.push(BurnError.collateralTooLow);
+    }
+
+    if (BurnFormValidation.isBelowDebtFloor(currentDebt, debtFloor)) {
+      errors.push(BurnError.debtTooLow);
+    }
+
+    return errors;
+  }
+
+  // currentDebt = Vat.ilk.rate * Vat.urn.art - daiToRepay
+  static getCurrentDebt(daiToRepay: FixedNumber, debt: FixedNumber, debtMultiplier: FixedNumber) {
+    const normalizedDebt = debtMultiplier.toFormat(format).mulUnsafe(debt.toFormat(format));
+    // Vat.ilk.rate * (Vat.urn.art) - daiAmount < Vat.ilk.spot * (Vat.urn.ink + collateralAmount)
+    return normalizedDebt.subUnsafe(daiToRepay.toFormat(format));
+  }
+
+  // daiBalance - daiToRepay < 0
+  static isInsufficientBalance(daiBalance: FixedNumber, daiToRepay: FixedNumber): boolean {
+    return daiBalance.toFormat(format).subUnsafe(daiToRepay.toFormat(format)).isNegative();
+  }
+
+  // normalizedDebt - daiToRepay < 0
+  // 有効小数点18桁にしてから計算する
+  static isOverRepaying(debt: FixedNumber, daiToRepay: FixedNumber, debtMultiplier: FixedNumber): boolean {
+    // ここのroundは繰上げだから大丈夫
+    const normalizedDebt = debtMultiplier
+      .toFormat(format)
+      .mulUnsafe(debt.toFormat(format))
+      .round(UnitFormats.WAD.decimals)
+      .toFormat(UnitFormats.WAD);
+    return normalizedDebt.subUnsafe(daiToRepay.toFormat(UnitFormats.WAD)).isNegative();
+  }
+
+  // Vat.urn.ink < collateralToFree
+  static isInvalidCollateralFreeAmount(lockedBalance: FixedNumber, collateralToFree: FixedNumber): boolean {
+    return lockedBalance.toFormat(format).subUnsafe(collateralToFree.toFormat(format)).isNegative();
+  }
+
+  // currentDebt < Vat.ilk.spot * (Vat.urn.ink + collateralAmount)
+  static isCollateralizationRatioTooLow(
+    lockedBalance: FixedNumber,
+    collateralToFree: FixedNumber,
+    currentDebt: FixedNumber,
+    price: FixedNumber,
+  ) {
+    const currentCollateralInDai = price
+      .toFormat(format)
+      .mulUnsafe(lockedBalance.toFormat(format).subUnsafe(collateralToFree.toFormat(format)));
+    return currentCollateralInDai.subUnsafe(currentDebt).isNegative();
+  }
+
+  // 0 < currentDebt < debtFloor
+  static isBelowDebtFloor(currentDebt: FixedNumber, debtFloor: FixedNumber) {
+    return !currentDebt.isNegative() && currentDebt.toFormat(format).subUnsafe(debtFloor.toFormat(format)).isNegative();
+  }
+}
